3495b1d316d440697ce438278fc0d6ead36758a6
himanshu tripathi <sunny.era@gmail.com>
2023-04-06 10:49:23 +0200

    Cleanup

--- .history/src/views/HomeView_20230403000451.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <ShowsSearch></ShowsSearch>
-  <ShowsList />
-</template>
-
-<script setup>
-import ShowsSearch from '@/components/ShowsSearch.vue';
-import ShowsList from '@/components/ShowsList.vue';
-</script>
